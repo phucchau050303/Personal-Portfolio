2b186284797a1fe08b04325b9155224fd8dd560d
Peter Hoang Chau <phcchu88@gmail.com>
2025-09-16 22:17:40 +1000

    finish Certificate section

---
+++ src/components/CertificateCard.jsx
@@ -0,0 +1,38 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+const CertificateCard = ({ link, issuer, certificateName, imageLink }) => (
+    <Card className="mb-3 certificate-card" 
+        style={{ 
+            maxWidth: '230px', 
+            margin: '0 auto', 
+            boxShadow: '0 4px 24px 0 rgba(180,200,255,0.35)', 
+            backgroundColor: '#212529',
+            color: 'white'
+        }}>
+        <Card.Img 
+            variant="top" 
+            className="rounded-circle mt-4 mb-1 mx-auto" 
+            src={imageLink} alt={certificateName} 
+            style={{maxWidth: "100px"}}/>
+        <Card.Body>
+            <Card.Title 
+                style={{ fontSize: '1.5rem', fontWeight: 'bold', height: '5rem' }}
+                className='text-center mb-2'>{certificateName}</Card.Title>
+            <Card.Subtitle 
+                style={{ height: '2rem' }}
+                className="mb-3 mt-2 text-center">{issuer}</Card.Subtitle>
+            <Button 
+                variant="primary" 
+                href={link} 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className='ms-3 me-3 d-flex justify-content-center'
+                style={{backgroundColor: '#3a889696', borderColor: '#3a889696'}}>
+                View Certificate
+            </Button>
+        </Card.Body>
+    </Card>
+);
+
+export default CertificateCard;
--- src/sections/Certificate.jsx
+++ src/sections/Certificate.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import CertificateCard from '../components/CertificateCard';
 
 const Certificate = () => (
   <section id="certificate" className="mb-5 mt-5">
     <p className='mb-3 text-center' style={{fontSize: '2.3rem', fontWeight: 'bold'}}>Certificate</p>
-    <ul>
-      <li>2025: C# Basic Certificate (HackerRank)</li>
-      <li>2024: Career Development Certificate (Swinburne University of Technology)</li>
-    </ul>
+    <Row className="mb-4 d-flex justify-content-center">
+        <Col md-4 className="text-center mb-5 mt-5">
+            <CertificateCard
+                link="https://www.dropbox.com/scl/fi/3tnzzitczwzxiv3udop98/Swinburne-Career-Development.jpeg?rlkey=7vj6z7azjuxa7g6bnazday6mo&st=h5x3n8rs&dl=0"
+                issuer="Swinburne University of Technology"
+                imageLink="src/assets/YlBGBBHR_400x400.jpg"
+                certificateName="Career Development Certificate"
+            />
+        </Col>
+        <Col md-4 className="text-center mb-5 mt-5">
+            <CertificateCard 
+                link="https://www.hackerrank.com/certificates/1e1d6a4f9ef1"
+                issuer="Hackerrank"
+                imageLink="src/assets/HackerRank_Icon-1000px.png"
+                certificateName="C# Basic Certificate"
+            />
+        </Col>
+        <Col md-4 className="text-center mb-5 mt-5">
+            <CertificateCard 
+                link="https://www.hackerrank.com/certificates/1e1d6a4f9ef1"
+                issuer="Hackerrank"
+                imageLink="src/assets/HackerRank_Icon-1000px.png"
+                certificateName="React Basic Certificate"
+            />
+        </Col>
+    </Row>
   </section>
 );
 
